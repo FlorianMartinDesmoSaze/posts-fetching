4ef511b2bed0fae426a73699a54825568f3a570c
florian <martinflorian2@gmail.com>
2022-11-03 18:23:01 +0100

    some styling done

--- src/App.js
+++ src/App.js
@@ -10,19 +10,24 @@ import Post from './Post'
 function App() {
   return (
     <div className="App">
-      <h2>Latest articles</h2>
       <BrowserRouter>
-          <nav className="nav">
+      {/* <div className="menu-btn">
+        <div className="menu-btn__burger"></div>
+      </div> */}
+        <nav className="nav-bar">
+          <Link to="/" className="logo">Streameo</Link>
+          <div className="nav-links">
             <Link to="/">Home</Link>
             <Link to="/posts">Posts</Link>
-          </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/posts" element={<Posts />} />
-          <Route path="/posts/:id" element={<Post />} />
-        </Routes>
+          </div>
+        </nav>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts" element={<Posts />} />
+            <Route path="/posts/:id" element={<Post />} />
+          </Routes>
       </BrowserRouter>
-    </div>
+    </div >
   );
 }
 
