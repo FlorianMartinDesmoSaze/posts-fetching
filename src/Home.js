7986d2a362747b0f900c880d393801d1b2cbfc1a
florian <martinflorian2@gmail.com>
2022-11-14 10:35:26 +0100

    last commit

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="home">
             <h2 className="home-title">Welcome to freedom city</h2>
             {/* <img src="http://via.placeholder.com/1200x200" alt="placeholder"></img> */}
-            <img src={Mountains} alt="icy pines"></img>
+            <img className="img-home" src={Mountains} alt="icy pines"></img>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aliquid maiores reprehenderit minus culpa saepe debitis, impedit a quisquam blanditiis pariatur quasi rerum assumenda repellat praesentium magni numquam. Reprehenderit optio rem vero, eum voluptatum quibusdam blanditiis at nesciunt, consectetur atque soluta, dolorem exercitationem magni temporibus officia explicabo quaerat iste natus ex corrupti.</p>
             <button className="btn-test">click & hold</button>
         </div>
